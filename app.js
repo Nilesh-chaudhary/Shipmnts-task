226575731be0629fae85f7cb87e128ce8608f396
Nilesh-chaudhary <nileshchaudhary532@gmail.com>
2023-08-21 11:24:39 +0530

    login done

---
+++ app.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/connectdb.js";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+const port = process.env.PORT || 80;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+//using middleware - CORS POLICY
+app.use(cors());
+
+// database connection
+connectDB(DATABASE_URL);
+
+//json
+app.use(express.json());
+
+//load route
+app.use("/api/user", userRoutes);
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(port);
+// app.listen(port, () => {
+//   console.log(`server listening at http://localhost:${port}`);
+// });
+// https://shipmnts-task.azurewebsites.net/
+
+// delete emailconfig
